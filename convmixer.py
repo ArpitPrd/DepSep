002f7288d190ba2171b70014e3e2288da3d0ea64
Deevyansh Khadria <devkhadria@gmail.com>
2024-05-11 14:04:33 +0530

    Update convmixer.py

--- convmixer.py
+++ convmixer.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+# hello there 
+# hello arpit
 class Residual(nn.Module):
     def __init__(self, fn):
         super().__init__()
