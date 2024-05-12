b32a55c18e432f05be42a528e58697aa4cf55bca
ArpitPrd <134731788+ArpitPrd@users.noreply.github.com>
2024-05-12 18:58:46 +0530

    Update main.py
    changed few directories

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from convmixer import AdapConvMixer
 from torchvision.transforms import ToTensor, ToPILImage
 import torch.optim as optim
 from train import training, validate
-from result2matfrmMDL import Result2Mat
+from result2mat import Result2Mat
 import h5py
 import matplotlib.pyplot as plt
 from train import batch_PSNR
@@ -28,9 +28,9 @@ def main():
     kernel_size = 5
     patch_size = 1
     
-#     train_patches = PreparePatches()
-#     print(len(train_patches))
-#     tr_dataloader = DataLoader(train_patches, batch_size=256, shuffle=True, num_workers=0)
+    train_patches = PreparePatches()
+    print(len(train_patches))
+    tr_dataloader = DataLoader(train_patches, batch_size=256, shuffle=True, num_workers=0)
     
     val_patches = PreparevalPatches()
     print(len(val_patches))
@@ -39,7 +39,7 @@ def main():
     #100 epochs-BGU , 300-Harvard, 500-Cave
     epochs = 100
     model_name = 'august'
-    checkpoint_dir = '/DATA1/user/Documents/Paper Aug 2022/data/BGU/checkpoints/'
+    checkpoint_dir = './data/BGU/checkpoints/'
     model_path = os.path.join(checkpoint_dir, ''.join([model_name, '-{:3d}-{:3d}-{:2d}-{:2d}.pth']))
     #model_path = os.path.join(checkpoint_dir, ''.join([model_name, '.pth']))
     best_model_path, best_psnr = '', -np.inf
@@ -47,7 +47,6 @@ def main():
 ###################################   MODEL    ##################################################
     
     model = AdapConvMixer(dim,depth,kernel_size,patch_size)
-    #model = AdapConvMixer(dim,depth,kernel_size,patch_size)
     #print(model)
     model.cuda()
 #####################################  LOSS-FUNCTION #############################################
@@ -70,23 +69,22 @@ def main():
     val_psnr = []
     test_psnr = []
     for epoch in range(epochs):
-#         print(f"Epoch {epoch+1} of {epochs}")
-#         train_epoch_psnr = training(model,loss_fn,  optimizer, tr_dataloader, epoch)
-#         ## train_psnr & val_psnr used for plotting
-#         train_psnr.append(train_epoch_psnr)
-#         print('train psnr',train_epoch_psnr)
+        print(f"Epoch {epoch+1} of {epochs}")
+        train_epoch_psnr = training(model,loss_fn,  optimizer, tr_dataloader, epoch)
+        ## train_psnr & val_psnr used for plotting
+        train_psnr.append(train_epoch_psnr)
+        print('train psnr',train_epoch_psnr)
         val_epoch_psnr = validate(model,loss_fn_mse, val_dataloader)
         val_psnr.append(val_epoch_psnr)
         print('val_epoch_psnr',val_epoch_psnr)
         
+        plt.plot(train_psnr,label='train')
         plt.plot(val_psnr,label='val')
-#         plt.plot(train_psnr,label='train')
         
     #saving best psnr 
         if val_epoch_psnr > best_psnr:
                 best_psnr = val_epoch_psnr
-                #best_model_path = model_path.format(epoch, val_epoch_psnr)
-                #best_model_path = model_path.format(epoch, val_epoch_psnr)
+                best_model_path = model_path.format(epoch, val_epoch_psnr)
                 torch.save(model.state_dict(), best_model_path)
                 print('Best PSNR: {:4f}'.format(best_psnr))
     
@@ -95,16 +93,9 @@ def main():
 ####################################### Result 2 mat ############################################################
     
     test_epoch_psnr = Result2Mat(val_dataloader, val_patches, best_model_path)
-    #print('test psnr',test_epoch_psnr)
-    #test_psnr.append(test_epoch_psnr)
+    print('test psnr',test_epoch_psnr)
+    test_psnr.append(test_epoch_psnr)
     
                 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
